476bcf55b692f7ba0cd00b3a2b0dc1da607454f8
ftr9 <ferlandodotel16@gmail.com>
2023-07-17 19:59:26 +0545

    refactor: Placed all the associated variables in global object

--- main.js
+++ main.js
@@ -1,60 +1,68 @@
 import './style.css';
 import WaveSurfer from 'wavesurfer.js';
-const nextButton = document.querySelector('.next_button');
-const prevButton = document.querySelector('.prev_button');
-const banner = document.querySelector('.banner');
+import songs from './data/musicData.json';
 
-let currentSong = 0;
-const songs = [
-  {
-    name: 'goodtime',
-    poster:
-      'https://images.pexels.com/photos/2932418/pexels-photo-2932418.jpeg?auto=compress&cs=tinysrgb&w=1260',
+const global = {
+  interactionButtons: {
+    nextButton: document.querySelector('.next_button'),
+    prevButton: document.querySelector('.prev_button'),
+    pauseButton: document.querySelector('.pause_button'),
   },
-  {
-    name: 'ownparadise',
-    poster:
-      'https://images.pexels.com/photos/10495915/pexels-photo-10495915.jpeg?auto=compress&cs=tinysrgb&w=1260',
+  overLays: {
+    banner: document.querySelector('.banner'),
+    bannerTitle: document.querySelector('.banner_title'),
   },
-  {
-    name: 'theweekend',
-    poster:
-      'https://images.pexels.com/photos/4571219/pexels-photo-4571219.jpeg?auto=compress&cs=tinysrgb&w=600',
+  variable: {
+    currentSongIdx: 0,
   },
-];
-const webSurfurConfig = {
-  container: '#waveform',
-  waveColor: '#495057',
-  progressColor: '#adb5bd',
-  autoCenter: true,
-  height: 80,
+  config: {
+    webSurfurConfig: {
+      container: '#waveform',
+      waveColor: '#495057',
+      progressColor: '#adb5bd',
+      autoCenter: true,
+      height: 80,
+    },
+  },
 };
+
 let wavesurfer = WaveSurfer.create({
-  ...webSurfurConfig,
-  url: '/music/goodtime.mp3',
+  ...global.config.webSurfurConfig,
+  url: `/music/${songs[0].name}.mp3`,
 });
-
-const playNextSong = async direction => {
-  direction === 'left' ? currentSong-- : currentSong++;
-  currentSong = Math.abs(currentSong);
-  console.log(currentSong % 3);
-  //create new instance
-  wavesurfer.destroy();
-  wavesurfer = WaveSurfer.create({ ...webSurfurConfig });
-  await wavesurfer.load(`/music/${songs[currentSong % 3].name}.mp3`);
-
-  //change the banner
-
-  banner.style.backgroundImage = `linear-gradient(to top,rgba(0, 0, 0, 0.801),rgba(0, 0, 0, 0.5)),
-    url('${songs[currentSong % 3].poster}')`;
-
-  wavesurfer.play();
-  wavesurfer.on('finish', () => playNextSong('right'));
-};
-
+global.overLays.bannerTitle.textContent = songs[0].name;
+global.overLays.banner.style.backgroundImage = `linear-gradient(to top,rgba(0, 0, 0, 0.801),rgba(0, 0, 0, 0.5)),
+    url('${songs[0].poster}')`;
 wavesurfer.on('interaction', () => {
   wavesurfer.play();
 });
 
-nextButton.addEventListener('click', () => playNextSong('right'));
-prevButton.addEventListener('click', () => playNextSong('left'));
+//All Buttons listeners
+global.interactionButtons.nextButton.addEventListener('click', () =>
+  playNextSong('right')
+);
+global.interactionButtons.prevButton.addEventListener('click', () =>
+  playNextSong('left')
+);
+
+const playNextSong = async direction => {
+  //change song index logic
+  let currentSongIndex = global.variable.currentSongIdx;
+  direction === 'left' ? currentSongIndex-- : currentSongIndex++;
+  currentSongIndex = Math.abs(currentSongIndex);
+  global.variable.currentSongIdx = currentSongIndex;
+
+  //Changing music instance
+  wavesurfer.destroy();
+  wavesurfer = WaveSurfer.create({ ...global.config.webSurfurConfig });
+  await wavesurfer.load(
+    `/music/${songs[currentSongIndex % songs.length].name}.mp3`
+  );
+
+  //change the banner
+  global.overLays.banner.style.backgroundImage = `linear-gradient(to top,rgba(0, 0, 0, 0.801),rgba(0, 0, 0, 0.5)),
+    url('${songs[currentSongIndex % songs.length].poster}')`;
+
+  wavesurfer.play();
+  wavesurfer.on('finish', () => playNextSong('right'));
+};
